376b87645f86e04d950c94dd54004f3cf1cde27a
baiken314 <43380463+baiken314@users.noreply.github.com>
2020-02-24 13:05:36 -0500

    Update webadv_roomsched.pl

--- CS371/Perl/webadv_roomsched.pl
+++ CS371/Perl/webadv_roomsched.pl
@@ -29,7 +29,8 @@ my $term = $ARGV[0];
 my $room = $ARGV[1];
 
 # get webpage
-my $url = "https://www2.monmouth.edu/muwebadv/wa3/search/SearchClassesV2.aspx";
+my $url = 
+"https://www2.monmouth.edu/muwebadv/wa3/search/SearchClassesV2.aspx";
 my $mech = WWW::Mechanize->new();
 $mech->get($url);
 
@@ -53,57 +54,24 @@ my @tds = split /<td>/, $page;
 
 my %course_hash; # stores all information on courses
 
-# 0 is default value until changed to add to course_hash
-my $current_course = 0;
-my $current_schedule = 0;
-my $current_room = 0;
+my $current_course;
 
 # regex check each <td> line and collect information on every course
 foreach my $td (@tds) {
 	
-	# get schedule
-	if ($td =~ /[0-9]+:[0-9]+[A-Z]+/) {
-		my @schedule_array = $td =~ 
-			/[A-Z]* +[A-Z]+ +[0-9]+:[0-9]+[A-Z]+ +[0-9]+:[0-9]+[A-Z]+/g;
-		$current_schedule = join "\n\t\t", @schedule_array;
-	}
-	
-	# print course name
+	# course name
 	if ($td =~ /[A-Z]+-[A-Z0-9]+-[A-Z0-9]+/) {
 		my @course_array = $td =~ /[A-Z]+-[A-Z0-9]+-[A-Z0-9]+/g;
-		$current_course = join "", @course_array;
+		$current_course = $course_array[0];
 	}
 	
-	# get room number
-	if ($td =~ /[A-Z]+ +[0-9A-Z]*[A-Z]*/) {
-		my @room_array = $td =~ /[A-Z]+ +[0-9A-Z]*[A-Z]* *[^MTWHF ]+/g;
-		$current_room = $room_array[0];
-		$current_room =~ s/\s+/ /g;
-	}
-	
-	# add course info to hashes when not 0
-	if ($current_course and $current_schedule and $current_room) {
-		$course_hash{$current_course}{room} = $current_room;
-		$course_hash{$current_course}{schedule} = $current_schedule;
-		# reset values so they are false
-		($current_course, $current_schedule, $current_room) = (0, 0, 0);
+	# get schedule and room
+	if ($current_course) {
+		# split 
+		foreach $br (split /<br>/ $td) {
+			
+		}
+		$current_couse = 0;
 	}
 	
 }
-
-# get courses only in specified room
-my @courses; # array for all classes in same room
-
-# search for all classes in a room, push to courses
-foreach my $key (sort keys %course_hash) {
-	if ($course_hash{$key}{room} =~ $room) { push @courses, $key; }
-}
-
-# print output of the schedule
-print "Schedule for $room:\n";
-if ($#courses + 1 > 0) { # if course found
-	foreach my $course (@courses) {
-		print "$course\t$course_hash{$course}{schedule}\n";
-	}
-}
-else { print "No results were found for this room.\n"; }
