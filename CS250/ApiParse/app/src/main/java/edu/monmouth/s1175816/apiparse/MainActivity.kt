73643aa1fa438efd18f76cf3036d4b5f2e6ff02c
baiken314 <43380463+baiken314@users.noreply.github.com>
2020-03-02 13:15:02 -0500

    Updated assignments

---
+++ CS250/ApiParse/app/src/main/java/edu/monmouth/s1175816/apiparse/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.monmouth.s1175816.apiparse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
