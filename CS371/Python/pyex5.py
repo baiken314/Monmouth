011aa49e47673cd1f099664838b457030c01766e
baiken314 <43380463+baiken314@users.noreply.github.com>
2020-03-10 18:44:45 -0400

    Updated assignments

---
+++ CS371/Python/pyex5.py
@@ -0,0 +1,19 @@
+# Bradley Aiken
+# pyex5.py
+# uses roster.txt
+
+import re
+
+roster = {}
+
+input_file = open('roster.txt', 'r')
+
+for student in input_file.readlines():
+    # split into last, first, email
+    [last, first, email] = re.split('\s*,\s*', student)
+    email = email.rstrip()  # remove newline
+    student_id = re.sub('@monmouth.edu', '', email)  # substitution
+    roster[student_id] = last + ', ' + first
+
+for key in roster.keys():
+    print(key, ', ', roster[key], sep='')
