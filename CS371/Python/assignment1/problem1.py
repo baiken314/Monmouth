5db8acb32cf2e599c0c168dee5ef571957718a25
baiken314 <43380463+baiken314@users.noreply.github.com>
2020-03-10 20:23:42 -0400

    Updated assignments

---
+++ CS371/Python/assignment1/problem1.py
@@ -0,0 +1,7 @@
+# progname.py
+verylongvarname = 10
+# increment verylongvarname by 1:
+verylongvarnaem = verylongvarname + 1
+
+# print incremented verylongvarname:
+print('verylongvarname:', verylongvarname)
