a51888c3b14c0f9d5b7e522c5a774ee3a6e38685
baiken314 <60582899+baiken314@users.noreply.github.com>
2020-05-01 17:42:29 -0400

    edited assignments

---
+++ CS250/Projects/CatEncyclopedia/app/src/main/java/edu/monmouth/s1175816/catencyclopedia/MainActivity.kt
@@ -0,0 +1,13 @@
+package edu.monmouth.s1175816.catencyclopedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
