7f53d8be22323df93487804112fd37c7053a254e
baiken314 <43380463+baiken314@users.noreply.github.com>
2021-04-07 20:28:07 -0400

    Update playerRouter.js

--- CS492/SeniorProject/routes/playerRouter.js
+++ CS492/SeniorProject/routes/playerRouter.js
@@ -114,26 +114,67 @@ router.route("/market-order").post(async (req, res) => {
 
 /**
  * req.body.game: game._id
- * req.body.attacker: player._id
- * req.body.originRegion: region._id  // of the region attacking from
- * req.body.targetRegion: region._id  // of the region being attacked
+ * req.body.player: player._id
+ * req.body.attackingRegion: region._id  // of the region attacking from
+ * req.body.defendingRegion: region._id  // of the region being attacked
  * req.body.units: Object  // units attacking with
  */
 router.route("/attack").post(async (req, res) => {
     console.log("POST player/attack");
     
     let game = await Game.findOne({ _id: req.body.game });
-    let player = game.players.filter(player => player._id == req.body.player)[0];
-});
 
-/**
- * req.body.game: game._id
- * req.body.defender: player._id
- * req.body.attacker: player._id
- * req.body.accept: Boolean  // does the player want to defend?
- */
-router.route("/defend").post(async (req, res) => {
+    let attackingRegion = game.regions.filter(region => region._id == req.body.attackingRegion);
+    let defendingRegion = game.regions.filter(region => region._id == req.body.defendingRegion);
 
+    let attacker = game.players.filter(player => player._id == req.body.player)[0];
+    let defender = game.players.filter(player => player._id == defendingRegion.player);
+
+    let units = req.body.units;
+
+    // check if regions are adjacent
+    if (!attackingRegion.adjacentRegionNames.includes(defendingRegion.name)) {
+        res.json({ message: "ERROR - regions are not adjacent" });
+    }
+
+    // check if attacker owns the region
+    else if (attackingRegion.player != player._id) {
+        res.json({ message: "ERROR - attacker does not control the region" });
+    }
+
+    else {
+        // check if attacking region has enough troops
+        for (unit in units) {
+            if (player._doc.units[unit] < units[unit]) {
+                res.json({ message: "ERROR - attacker does not have enough " + [unit] });
+                return;
+            }
+        }
+
+        // check if defending region has any troops
+        if (defendingRegion.units.land == 0 &&
+            defendingRegion.units.naval == 0 &&
+            defendingRegion.units.amphibious == 0) {
+            res.json({ message: "ERROR - defending region has no defending units" });
+            return;
+        }
+
+        // check if attacker has resources to attack
+        if (attacker.resources.agriculture < game.market.costs.attacking.resources.agriculture ||
+            attacker.resources.mining < game.market.costs.attacking.resources.mining || 
+            attacker.resources.synthetics < game.market.costs.attacking.resources.synthetics) {
+            res.json({ message: "ERROR - player does not have enough resources to attack" });
+            return;
+        }
+
+        // check if defender has resources to defend
+        let attackerDice = 1;
+        let defenderDice = 1;
+        
+        // check which troops can attack
+
+        // roll dice to determine results   
+    }
 });
 
 /**
